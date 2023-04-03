74161effcdcb4a77008d1527aba9df2f4c3cb0e4
DESKTOP-34IA31U\rxlee <ryan.lee@educatorimpact.com>
2023-04-03 13:02:36 +0800

    Limit to non waiting lobbies

--- commands/main/rating.js
+++ commands/main/rating.js
@@ -44,9 +44,7 @@ async function getRating(interaction, noCache) {
   await interaction.guild.members.fetch();
   let channels = interaction.guild.channels.cache;
   channels = channels.filter((c) =>
-    ["RED", "BLU", "BLUE", "WAITING", "RED-2", "BLUE-2", "WAITING-2"].includes(
-      c.name
-    )
+    ["RED", "BLU", "BLUE", "RED-2", "BLUE-2"].includes(c.name)
   );
 
   const teamRatings = [];
