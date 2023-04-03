2dc1f3ea2b805cd46c172516a3b81e49709f2422
Ryan Lee <ryan.lee@educatorimpact.com>
2023-04-03 12:23:41 +0800

    Add player swap suggestion

--- events/voiceStateUpdate.js
+++ events/voiceStateUpdate.js
@@ -38,6 +38,15 @@ module.exports = {
 
         const botMessageChannel = channels.find((c) => c.name === "bot-test");
         botMessageChannel.send(convertRatingToString(rating));
+
+        if (
+          rating.length > 1 &&
+          Math.abs(
+            +rating[0][rating[0].length - 1] - +rating[1][rating[1].length - 1]
+          ) > 25
+        ) {
+          botMessageChannel.send(suggestPlayerSwap(rating));
+        }
       }
     }
   },
@@ -51,4 +60,54 @@ function offCooldown() {
     return true;
   }
   return false;
+}
+
+// for each team
+// pick a player from the higher team closest to the average plus half the difference between teams
+// suggest swap with player from other team with score closest to the average of their team subtract half the difference
+function suggestPlayerSwap(rating) {
+  const teamA = [...rating[0]];
+  const teamB = [...rating[1]];
+  const teamAScore = +teamA[teamA.length - 1];
+  const teamBScore = +teamB[teamB.length - 1];
+
+  let highTeam, highTeamScore;
+  let lowTeam, lowTeamScore;
+
+  if (teamAScore > teamBScore) {
+    highTeam = teamA;
+    lowTeam = teamB;
+  } else {
+    highTeam = teamB;
+    lowTeam = teamA;
+  }
+  highTeamScore = +highTeam[highTeam.length - 1];
+  lowTeamScore = +lowTeam[lowTeam.length - 1];
+
+  let highTeamTarget = highTeamScore / 6 + (highTeamScore - lowTeamScore) / 2;
+  let lowTeamTarget = lowTeamScore / 6 - (highTeamScore - lowTeamScore) / 2;
+
+  let highTeamPlayer = highTeam[0];
+  let lowTeamPlayer = lowTeam[0];
+
+  highTeam.forEach((player) => {
+    if (
+      player.rating > highTeamTarget &&
+      Math.abs(player.rating - highTeamTarget) <
+        Math.abs(highTeamPlayer - highTeamTarget)
+    ) {
+      highTeamPlayer = player;
+    }
+  });
+  lowTeam.forEach((player) => {
+    if (
+      player.rating < lowTeamTarget &&
+      Math.abs(player.rating - lowTeamTarge) <
+        Math.abs(lowTeamPlayer - lowTeamTarget)
+    ) {
+      lowTeamPlayer = player;
+    }
+  });
+
+  return `Teams look a bit imbalanced!\nMaybe swap ${highTeamPlayer.name} and ${lowTeamPlayer.name}?`;
 }
