d6989383953d3cf9956855d02cb301d5b835ebd9
Ryan Lee <ryan.lee@educatorimpact.com>
2023-04-16 22:19:04 +0800

    add roll

---
+++ commands/main/roll.js
@@ -0,0 +1,30 @@
+const { SlashCommandBuilder } = require("discord.js");
+
+module.exports = {
+  data: new SlashCommandBuilder()
+    .setName("roll")
+    .setDescription("Roll captains")
+    .addIntegerOption((option) =>
+      option.setName("players").setDescription("Number of players")
+    )
+    .addIntegerOption((option) =>
+      option.setName("rolls").setDescription("Number of rolls")
+    ),
+  async execute(interaction) {
+    await interaction.deferReply();
+
+    const numPlayers = interaction.options.getInteger("players");
+    const numRolls = interaction.options.getInteger("rolls");
+
+    const rolls = [];
+
+    while (rolls.length < numRolls) {
+      let maybeRoll = Math.ceil(Math.random() * numPlayers);
+      if (!rolls.includes(maybeRoll)) {
+        rolls.push(maybeRoll);
+      }
+    }
+
+    await interaction.editReply(`${rolls.join(", ")}`);
+  },
+};
