49d2c631d18f980b6c5250bac6b8c790a1bffa1b
DESKTOP-34IA31U\rxlee <ryan.lee@educatorimpact.com>
2023-06-15 14:40:57 +0800

    clear nominations

---
+++ commands/main/clearnominations.js
@@ -0,0 +1,22 @@
+const { SlashCommandBuilder } = require("discord.js");
+const { currentVoteMaps } = require("./votemap");
+
+module.exports = {
+  data: new SlashCommandBuilder()
+    .setName("clearnominations")
+    .setDescription("Clear current nominations"),
+  async execute(interaction) {
+    await interaction.deferReply();
+
+    const defaultMaps = mapPool.filter(
+      (m) =>
+        ["cp_snakewater", "cp_sunshine", "cp_gullywash"].includes(m.name) &&
+        !currentVoteMaps.some((c) => c.name === m.name)
+    );
+    currentVoteMaps = [...defaultMaps];
+
+    console.log(currentVoteMaps);
+
+    await interaction.editReply(`Done.`);
+  },
+};
